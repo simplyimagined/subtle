551481702a382a184e96f35a6aa036856109eba5
Dan Anderson <dan@simplyimagined.com>
2023-06-14 16:28:21 -0500

    Moved function out of Puzzle and into Guess

---
+++ lib/subtle/guess.ex
@@ -0,0 +1,157 @@
+defmodule Subtle.Guess do
+  @moduledoc """
+  Documentation for `Guess`.
+  Functions for guesses and letter comparisons.
+  """
+#  alias __MODULE__
+
+#I have no idea how to typedef what I want, so dynamic it is!
+#  @enforce_keys [:guess]
+#  defstruct [:guess, :results]
+
+ # @letter_position [:correct, :wrong_position, :wrong_letter]
+
+#  @type guess_result_tuple :: {
+#    letter: String.t,
+#    result: atom()
+#  }
+
+  @doc """
+  guess_word/2
+
+  ## Examples
+      # guess apple for paper
+      iex> Guess.guess_word("apple", "paper")
+      {:ok, [ {"a", :wrong_position}, {"p", :wrong_position},
+              {"p", :correct}, {"l", :wrong_letter},
+              {"e", :wrong_position} ]}
+      # guess paper for paper
+      iex> Guess.guess_word("paper", "paper")
+      {:ok, correct}
+  """
+  def guess_word(guess, answer)
+  when is_binary(guess) and is_binary(answer) and guess == answer
+  do
+    {:ok, :correct}
+  end
+  def guess_word(guess, answer) when is_binary(guess) and is_binary(answer)
+  do
+    answer_len = String.length(answer)
+    case String.length(guess) do
+      ^answer_len -> {:ok, compare_letters(guess, answer)}
+      _           -> {:error, :invalid_length}
+    end
+  end
+  def guess_word(_guess, _answer) do
+    {:error, :invalid_arguments}
+  end
+
+  @doc """
+  compare_letters/2
+
+  ## Examples
+      # guess apple for paper
+      iex> Guess.compare_letters("apple", "paper")
+      [ {"a", :wrong_position}, {"p", :wrong_position},
+        {"p", :correct}, {"l", :wrong_letter},
+        {"e", :wrong_position} ]
+  """
+  @spec compare_letters(String.t, String.t) :: list
+  def compare_letters(guess, answer) do
+    # guess = "apple", answer = "paper"
+    # make into grapheme lists:
+    # ["a", "p", "p", "l", "e"], ["p", "a", "p", "e", "r"]
+    # zip these together:
+    # -> [{"a", "p"}, {"p", "a"}, {"p", "p"}, {"l", "e"}, {"e", "r"}]
+    # map_reduce:
+    # {"a", "p"}, %{"a" => 1, "e" => 1, "p" => 2, "r" => 1} -> {"a", :wrong_position}
+    # {"p", "a"}, %{"a" => 0, "e" => 1, "p" => 2, "r" => 1} -> {"p", :wrong_position}
+    # {"p", "p"}, %{"a" => 0, "e" => 1, "p" => 1, "r" => 1} -> {"p", :correct}
+    # {"l", "e"}, %{"a" => 0, "e" => 1, "p" => 0, "r" => 1} -> {"p", :wrong_letter}
+    # {"e", "r"}, %{"a" => 0, "e" => 1, "p" => 0, "r" => 1} -> {"p", :wrong_position}
+    #
+    # answers =
+    #   [{"a", :wrong_position}, {"p", :wrong_position}, {"p", :correct},
+    #     {"l", :wrong_letter}, {"e", :wrong_position}]
+    #
+    answer_letters = String.graphemes(answer)
+    guess_letters = String.graphemes(guess)
+
+    {answers, _counts} =
+      Enum.zip(guess_letters, answer_letters)
+      |> Enum.map_reduce(letter_counts(answer),
+          fn pair, counts -> process_letter_pair(pair, counts) end)
+
+    # return only the answers, not the counts
+    answers
+  end
+
+  @doc """
+  process_letter_pair/2
+
+  ## Examples
+      # guess apple for paper, first char
+      iex> Guess.process_letter_pair("a", "p", letter_counts(paper))
+      {"a", :wrong_position}
+      # guess apple for paper, third char
+      iex> Guess.process_letter_pair("p", "p", letter_counts(paper))
+      {"p", :correct}
+
+  """
+  def process_letter_pair({_a, _b} = pair, counts) do
+    case compare_letter_pair(pair) do
+      # they match, return success
+      {a, :correct} ->
+        {{a, :correct}, reduce_letter_count(counts, a)}
+      # they don't match, but the letter may be in the puzzle elsewhere
+      {a, :miss} ->
+        cond do
+          # check if the letter is in the puzzle elsewhere
+          is_number(counts[a]) and counts[a] > 0 ->
+            {{a, :wrong_position}, reduce_letter_count(counts, a)}
+          true ->
+            {{a, :wrong_letter}, counts}
+        end
+    end
+  end
+
+  defp compare_letter_pair({a, b}) when a == b, do: {a, :correct}
+  defp compare_letter_pair({a, _b}), do: {a, :miss}
+
+   @doc """
+  letter_counts/1
+
+  ## Examples
+
+      iex> Guess.letter_counts("apple")
+      %{"a" => 1, "e" => 1, "l" => 1, "p" => 2}
+
+  """
+  def letter_counts(word) do
+    word
+    |> String.graphemes()
+    |> Enum.reduce(%{}, fn char, acc ->
+          Map.put(acc, char, (acc[char] || 0) + 1)
+        end)
+  end
+
+   @doc """
+  reduce_letter_count/2
+
+  ## Examples
+
+      iex> apple_counts = letter_counts("apple")
+      %{"a" => 1, "e" => 1, "l" => 1, "p" => 2}
+      iex> Guess.reduce_letter_count(apple_counts, "e")
+      %{"a" => 1, "e" => 0, "l" => 1, "p" => 2}
+
+  """
+  def reduce_letter_count(letter_counts, reduced_letter) do
+    letter_counts
+    |> Enum.reduce(%{}, fn {letter, count} , acc  ->
+      Map.put(acc, letter, adjust_count(letter, reduced_letter, count)) end)
+  end
+
+  defp adjust_count(a, b, count) when a == b and count > 0, do: count - 1
+  defp adjust_count(_a, _b, count), do: count
+end
--- test/subtle/guess_test.exs
+++ test/subtle/guess_test.exs
@@ -1,79 +1,65 @@
-defmodule Subtle.PuzzleTest do
+defmodule Subtle.GuessTest do
   use ExUnit.Case, async: true
-  alias Subtle.Puzzle
+  alias Subtle.Guess
 
   describe "letter_counts/1" do
     test "letter counts for input 'paper'" do
-      assert Puzzle.letter_counts("paper") ==
+      assert Guess.letter_counts("paper") ==
         %{"a" => 1, "e" => 1, "p" => 2, "r" => 1}
     end
 
     test "letter counts for input 'zebra'" do
-      assert Puzzle.letter_counts("zebra") ==
+      assert Guess.letter_counts("zebra") ==
         %{"a" => 1, "b" => 1, "e" => 1, "r" => 1, "z" => 1}
     end
   end
 
   describe "reduce_letter_count/2" do
     test "reduce count for p in pizza" do
-      pizza_counts = Puzzle.letter_counts("pizza")
-      assert Puzzle.reduce_letter_count(pizza_counts, "p") ==
+      pizza_counts = Guess.letter_counts("pizza")
+      assert Guess.reduce_letter_count(pizza_counts, "p") ==
         %{"a" => 1, "i" => 1, "p" => 0, "z" => 2}
     end
 
     test "reduce count for e in pizza" do
-      pizza_counts = Puzzle.letter_counts("pizza")
-      assert Puzzle.reduce_letter_count(pizza_counts, "e") ==
+      pizza_counts = Guess.letter_counts("pizza")
+      assert Guess.reduce_letter_count(pizza_counts, "e") ==
         %{"a" => 1, "i" => 1, "p" => 1, "z" => 2}
     end
 
     test "reduce count for z in pizza several times" do
-      pizza_counts = Puzzle.letter_counts("pizza")
+      pizza_counts = Guess.letter_counts("pizza")
       new_counts =
-        Puzzle.reduce_letter_count(pizza_counts, "z")
-        |> Puzzle.reduce_letter_count("z")
-        |> Puzzle.reduce_letter_count("z")
+        Guess.reduce_letter_count(pizza_counts, "z")
+        |> Guess.reduce_letter_count("z")
+        |> Guess.reduce_letter_count("z")
       assert new_counts == %{"a" => 1, "i" => 1, "p" => 1, "z" => 0}
-    end
-  end
-
-  describe "compare_letter_pair/1" do
-    test "when letters match" do
-      assert Puzzle.compare_letter_pair({"a", "a"}) == {"a", :correct}
-    end
-
-    test "when letters don't match" do
-      assert Puzzle.compare_letter_pair({"z", "a"}) == {"z", :miss}
-    end
-
-    test "when input is wrong" do
-      assert Puzzle.compare_letter_pair({:foo, "a"}) == {:foo, :miss}
     end
   end
 
   describe "process_letter_pair/2" do
     test "when letter matches" do
-      counts = Puzzle.letter_counts("pizza")
-      assert Puzzle.process_letter_pair({"z", "z"}, counts) ==
+      counts = Guess.letter_counts("pizza")
+      assert Guess.process_letter_pair({"z", "z"}, counts) ==
         {{"z", :correct}, %{"a" => 1, "i" => 1, "p" => 1, "z" => 1}}
     end
 
     test "when letter doesn't match but is in answer" do
-      counts = Puzzle.letter_counts("pizza")
-      assert Puzzle.process_letter_pair({"a", "z"}, counts) ==
+      counts = Guess.letter_counts("pizza")
+      assert Guess.process_letter_pair({"a", "z"}, counts) ==
         {{"a", :wrong_position}, %{"a" => 0, "i" => 1, "p" => 1, "z" => 2}}
     end
 
     test "when letter isn't in answer" do
-      counts = Puzzle.letter_counts("pizza")
-      assert Puzzle.process_letter_pair({"q", "z"}, counts) ==
+      counts = Guess.letter_counts("pizza")
+      assert Guess.process_letter_pair({"q", "z"}, counts) ==
         {{"q", :wrong_letter}, %{"a" => 1, "i" => 1, "p" => 1, "z" => 2}}
     end
   end
 
   describe "compare_letters/2" do
     test "compare apple and paper" do
-      assert Puzzle.compare_letters("apple", "paper") ==
+      assert Guess.compare_letters("apple", "paper") ==
         [
           {"a", :wrong_position},
           {"p", :wrong_position},
@@ -84,7 +70,7 @@ defmodule Subtle.PuzzleTest do
     end
 
     test "compare apple and tuple" do
-      assert Puzzle.compare_letters("apple", "tuple") ==
+      assert Guess.compare_letters("apple", "tuple") ==
         [
           {"a", :wrong_letter},
           {"p", :wrong_position},
@@ -97,11 +83,11 @@ defmodule Subtle.PuzzleTest do
 
   describe "guess_word/2" do
     test "guess apple for apple" do
-      assert Puzzle.guess_word("apple", "apple") == {:ok, :correct}
+      assert Guess.guess_word("apple", "apple") == {:ok, :correct}
     end
 
     test "guess apple for paper" do
-      assert Puzzle.guess_word("apple", "paper") ==
+      assert Guess.guess_word("apple", "paper") ==
         {:ok,
           [
             {"a", :wrong_position},
@@ -113,11 +99,11 @@ defmodule Subtle.PuzzleTest do
     end
 
     test "mismatched word length" do
-      assert Puzzle.guess_word("audacious", "pizza") == {:error, :invalid_length}
+      assert Guess.guess_word("audacious", "pizza") == {:error, :invalid_length}
     end
 
     test "invalid inputs" do
-      assert Puzzle.guess_word(100, "pizza") == {:error, :invalid_arguments}
+      assert Guess.guess_word(100, "pizza") == {:error, :invalid_arguments}
     end
   end
 end
