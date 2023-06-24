23329682cb66d38419e9a274fedef272ca32a068
Dan Anderson <dan@simplyimagined.com>
2023-06-24 14:04:13 -0500

    New puzzle length, put a message for each guess, cleaner code

--- lib/subtle/puzzle.ex
+++ lib/subtle/puzzle.ex
@@ -5,12 +5,15 @@ defmodule Subtle.Puzzle do
 
   alias Subtle.{Puzzle, Guess}
 
-  @max_guesses 5
+  @max_guesses 6
+  @word_length 5
 
   @enforce_keys [:state, :answer, :guesses]
   defstruct [
     state: :playing,
     answer: "",
+    message: "",
+    word_length: @word_length,
     max_guesses: @max_guesses,
     guesses: []
   ]
@@ -19,42 +22,66 @@ defmodule Subtle.Puzzle do
     Puzzle.new(random_puzzle_word())
   end
   def new(answer) do
-    %Puzzle{state: :playing, answer: answer,
-             max_guesses: @max_guesses, guesses: []}
+    %Puzzle{state: :playing,
+              answer: answer, word_length: @word_length,
+              message: "Guess a word.",
+              max_guesses: @max_guesses, guesses: []}
   end
 
   def make_guess(%Puzzle{state: :playing} = puzzle, guess) do
     case Guess.guess_word(guess, puzzle.answer) do
       {:ok, :correct, guess_result} ->
         # correct guess will transition the puzzle to :game_won
-        {:ok, change_state_and_add_guess(puzzle, :game_won, guess_result)}
+        {:ok, puzzle
+              |> change_state(:game_won)
+              |> change_message("You won!")
+              |> add_result(guess_result)}
+#        change_state_and_add_guess(puzzle, :game_won, guess_result)}
       {:ok, :incorrect, guess_result} ->
         # incorrect guess will update the puzzle and check for :game_over
         case last_guess?(puzzle) do
           true ->
-            {:ok, change_state_and_add_guess(puzzle, :game_over, guess_result)}
+            {:ok, puzzle
+                  |> change_state(:game_over)
+                  |> change_message("Game over!")
+                  |> add_result(guess_result)}
+#            {:ok, change_state_and_add_guess(puzzle, :game_over, guess_result)}
           false ->
-            {:ok, add_guess(puzzle, guess_result)}
+            {:ok, puzzle
+                  |> change_message("Guess a different word.")
+                  |> add_result(guess_result)}
+#            {:ok, add_guess(puzzle, guess_result)}
         end
       # don't pass in garbage!
-      {:error, :invalid_length} -> {:error, puzzle, :invalid_length}
-      {:error, _} -> {:error, puzzle, :baby_dont_hurt_me}
+      {:error, :invalid_length} ->
+        {:error,
+          change_message(puzzle, "Guess must be #{puzzle.word_length} letters"),
+          :invalid_length}
+      {:error, _} ->
+        {:error,
+          change_message(puzzle, "Enter a #{puzzle.word_length} letter word"),
+          :baby_dont_hurt_me}
     end
   end
   def make_guess(puzzle, _), do: {:error, puzzle, :game_over}
 
-
-  def change_state_and_add_guess(puzzle, state, guess_result) do
-    # this works but I feel it's not the 'right' way
-    %Puzzle{puzzle | state: state}
-    |> add_guess(guess_result)
+  def change_state(puzzle, state), do: %{puzzle | state: state}
+  def change_message(puzzle, message), do: %{puzzle | message: message}
+  def add_result(puzzle, result) do
+    %{puzzle | guesses: puzzle.guesses ++ [result]}
   end
 
-  def add_guess(puzzle, guess_result) do
-    # this works but I feel it's not the 'right' way
-    %Puzzle{state: puzzle.state, answer: puzzle.answer,
-      guesses: List.insert_at(puzzle.guesses, -1, guess_result)}
-  end
+#  def change_state_and_add_guess(puzzle, state, guess_result) do
+#    # this works but I feel it's not the 'right' way
+#    %Puzzle{puzzle | state: state}
+#    |> add_guess(guess_result)
+#  end
+
+ # def add_guess(puzzle, guess_result) do
+ #   # this works but I feel it's not the 'right' way
+ #   %Puzzle{state: puzzle.state, answer: puzzle.answer,
+ #     guesses: List.insert_at(puzzle.guesses, -1, guess_result)}
+ # end
 
   @doc"""
   Returns true if we haven't exceeded the maximum guesses
@@ -83,21 +110,14 @@ defmodule Subtle.Puzzle do
   end
 
   def empty_guesses(puzzle) do
-    Enum.reduce(1..Puzzle.guesses_remaining(puzzle),
-      [], fn _i, acc -> [empty_guess(puzzle) | acc] end)
+    {:ok, result} = Guess.empty_guess(puzzle.answer)
+    List.duplicate(result, Puzzle.guesses_remaining(puzzle))
   end
 
-  def empty_guess(puzzle) do
-    answer_letters = String.graphemes(puzzle.answer)
-    results = Enum.reduce(
-      answer_letters, [],
-      fn _x, acc -> [{" ", :none} | acc] end)
-    %Guess{guess: String.duplicate(" ", Enum.count(answer_letters)),
-      results: results}
-  end
 
   @doc """
   This will return a string describing the current state of the game.
+  (Pretty much used for debugging)
   It shows the current state, the answer to the puzzle,
   and a list of any guesses made so far. Each guess will show
   a '!' before any wrong letter and '~' to show a correct letter
