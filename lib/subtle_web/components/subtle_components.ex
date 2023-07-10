3ccf62c380627b0240368c9d585411c41c72af06
Dan Anderson <imagine@simplyimagined.com>
2023-07-10 13:10:02 -0500

    Made my own modal to see how it works. Will toss.

--- lib/subtle_web/components/subtle_components.ex
+++ lib/subtle_web/components/subtle_components.ex
@@ -2,6 +2,10 @@ defmodule SubtleWeb.SubtleComponents do
   use Phoenix.Component
   use Phoenix.HTML
 
+  alias Phoenix.LiveView.JS
+  alias SubtleWeb.CoreComponents
+  import SubtleWeb.Gettext
+
   attr :letter, :string, required: true
   attr :hint, :atom, values: [:correct, :wrong_letter, :wrong_position, :used, :unused, :none], default: :none
   attr :kind, :atom, values: [:guess, :key], required: true
@@ -71,4 +75,70 @@ defmodule SubtleWeb.SubtleComponents do
       true -> :unused
     end
   end
+
+  attr :modal_id, :string, required: true
+  attr :icon, :string, required: true
+
+  def hero_modal_button(assigns) do
+    ~H"""
+      <button phx-click={CoreComponents.show_modal(@modal_id)}>
+        <CoreComponents.icon name={@icon} class="icon" />
+      </button>
+    """
+  end
+
+
+  attr :id, :string, required: true
+  attr :show, :boolean, default: false
+  attr :on_cancel, JS, default: %JS{}
+  slot :inner_block, required: true
+
+  def subtle_modal(assigns) do
+    ~H"""
+    <div
+      id={@id}
+      phx-mounted={@show && CoreComponents.show_modal(@id)}
+      phx-remove={CoreComponents.hide_modal(@id)}
+      data-cancel={JS.exec(@on_cancel, "phx-remove")}
+      class="relative z-50 hidden"
+    >
+      <div id={"#{@id}-bg"} class="bg-zinc-400/50 fixed inset-0 transition-opacity" aria-hidden="true" />
+      <div
+        class="fixed inset-0 overflow-y-auto"
+        aria-labelledby={"#{@id}-title"}
+        aria-describedby={"#{@id}-description"}
+        role="dialog"
+        aria-modal="true"
+        tabindex="0"
+      >
+        <div class="flex min-h-full items-center justify-center">
+          <div class="w-full max-w-3xl p-4 sm:p-6 lg:py-8">
+            <.focus_wrap
+              id={"#{@id}-container"}
+              phx-window-keydown={JS.exec("data-cancel", to: "##{@id}")}
+              phx-key="escape"
+              phx-click-away={JS.exec("data-cancel", to: "##{@id}")}
+              class="shadow-zinc-700/10 ring-zinc-700/10 relative hidden rounded-2xl bg-slate-700 p-14 shadow-lg ring-1 transition"
+            >
+              <div class="absolute top-6 right-5">
+                <button
+                  phx-click={JS.exec("data-cancel", to: "##{@id}")}
+                  type="button"
+                  class="-m-3 flex-none p-3 opacity-60 hover:opacity-90"
+                  aria-label={gettext("close")}
+                >
+                  <CoreComponents.icon name="hero-x-mark-solid" class="h-5 w-5 bg-zinc-200" />
+                </button>
+              </div>
+              <div id={"#{@id}-content"}>
+                <%= render_slot(@inner_block) %>
+              </div>
+            </.focus_wrap>
+          </div>
+        </div>
+      </div>
+    </div>
+    """
+  end
+
 end
