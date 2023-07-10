d013b21ef41f4c12ade5dcf097e64a76e828d124
Dan Anderson <imagine@simplyimagined.com>
2023-07-10 13:07:08 -0500

    modals, mucho changes

--- lib/subtle_web/subtle_live.ex
+++ lib/subtle_web/subtle_live.ex
@@ -1,6 +1,7 @@
 defmodule SubtleWeb.SubtleLive do
   use SubtleWeb, :live_view
   use Phoenix.Component
+  import SubtleWeb.SubtleComponents
   alias Subtle.Game
   alias SubtleWeb.SubtleComponents
 
@@ -9,25 +10,65 @@ defmodule SubtleWeb.SubtleLive do
                   page_title: "Subtle",
                   session_id: session["live_socket_id"],
                   game: Game.new(),
-                  message: "Subtle"
+                  message: "Subtle",
+                  settings: to_form(%{verify: true})
                 )}
   end
 
   def render(assigns) do
-# put after <div relative
-#    <Heroicons.cog_8_tooth mini class="absolute top-5 right-5 w-6 h-6 fill-zinc-200"
+#<h2 class="text-2xl text-zinc-200 mb-4">
+#<%= @message %>
+#</h2>
+    #    <Heroicons.cog_8_tooth mini class="absolute top-5 right-5 w-6 h-6 fill-zinc-200"
 #    phx-click={show_modal("settings-modal-id")} />
     ~H"""
-    <h2 class="text-2xl text-zinc-200 mb-4">
-      <%= @message %>
-    </h2>
+    <div class="app_header">
+      <div class="flex justify-start gap-x-2">
+        <.hero_modal_button modal_id="legend-modal" icon="hero-list-bullet" />
+        <.hero_modal_button modal_id="help-modal" icon="hero-question-mark-circle" />
+      </div>
+      <h1>Subtle</h1>
+      <div class="flex justify-end gap-x-2">
+        <.hero_modal_button modal_id="quit-modal" icon="hero-face-frown" />
+        <.hero_modal_button modal_id="settings-modal" icon="hero-cog-6-tooth" />
+      </div>
+    </div>
+
+    <.subtle_modal id="legend-modal">
+      <%= legend(assigns) %>
+    </.subtle_modal>
+    <.subtle_modal id="help-modal">
+      <div class="possible_words">
+        <h1>Possible words:</h1>
+        <p><%= available_answers(@game) %></p>
+      </div>
+    </.subtle_modal>
+    <.subtle_modal id="quit-modal">
+      <form class="give_up" phx-submit="give_up">
+        <h1>Game over, man!</h1>
+        <p>I tried really, really hard but this isn't working out for me. I'd like to quit the gym.</p>
+        <.button phx-click={hide_modal("quit-modal")}>
+          I give up
+          <.icon name="hero-face-frown-mini" class="ml-1 w-6 h-6" />
+        </.button>
+      </form>
+    </.subtle_modal>
+    <.modal id="settings-modal">
+      <.form for={@settings} phx-submit="settings">
+        <.input type="checkbox" id="settings-verify"
+            field={@settings[:verify]}
+            label="Require guess to be in the dictionary"
+            checked={@game.verify_guesses} />
+        <p>&nbsp;</p>
+        <.button phx-click={hide_modal("settings-modal")}>
+          Save
+        </.button>
+      </.form>
+    </.modal>
+
+
     <div class="grid grid-flow-row gap-y-4">
       <div class="relative flex rounded-lg p-2 bg-slate-700/50">
-        <.modal id="settings-modal-id">
-          <p>Inner Modal Content</p>
-          <form id="settings_form" phx-submit="save">
-          </form>
-        </.modal>
 
         <%= if Game.state(@game) == :playing do %>
           <.guess_form message={Game.message(@game)} verify={@game.verify_guesses} />
@@ -37,16 +78,11 @@ defmodule SubtleWeb.SubtleLive do
       </div>
 
       <%= render_guesses(assigns) %>
-      <%= render_legend(assigns) %>
-
-      <div class="possible_words">
-        <h1>Possible words:</h1>
-        <p><%= Game.available_words_as_string(@game) %></p>
-      </div>
 
     </div>
     """
-#    <p class="mt-30 text-zinc-200"> <%= Game.summary @game %> </p>
+#      <%= render_legend(assigns) %>
+      #    <p class="mt-30 text-zinc-200"> <%= Game.summary @game %> </p>
   end
 
   attr :message, :string, default: ""
@@ -57,29 +93,31 @@ defmodule SubtleWeb.SubtleLive do
   def guess_form(assigns) do
 #    IO.puts("guess_form.assigns")
 #    IO.inspect(assigns)
+#me <div class="flex space-x-4 auto-rows-max items-center">
+#them <div class="flex items-center justify-between border-b border-zinc-100 py-3 text-sm">
     ~H"""
     <form id="guess_form" phx-submit="guess">
       <div class="p-4 space-y-4">
         <label class="block text-xl font-medium text-zinc-200">
           <%= @message %>
         </label>
-        <div class="flex space-x-4 auto-rows-max items-center">
-          <input type="text" name="guess"
+        <div class="flex items-center justify-between space-x-4">
+          <input type="text" id="guess" name="guess"
             autofocus autocomplete="off"
             value={Phoenix.HTML.Form.normalize_value("text", @value)}
             class={[
-              "block w-full rounded-lg text-zinc-900 sm:text-sm sm:leading-6",
+              "block w-full rounded-lg text-zinc-900",
+              "text-md md:text-xl leading-4 md:leading-6",
               "phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400",
               "border-zinc-300 focus:border-zinc-400",
               @errors != [] && "border-rose-400 focus:border-rose-400"
             ]}
           />
-          <.button>Guess</.button>
+          <.button class="whitespace-nowrap">
+            Guess
+            <.icon name="hero-question-mark-circle-mini" class="ml-1 w-6 h-6" />
+          </.button>
         </div>
-        <label class="flex items-center gap-4 leading-6 text-sm md:text-base text-zinc-200">
-          <input type="checkbox" name="verify" value="true" checked={@verify} />
-          Require guess to be in the dictionary
-        </label>
       </div>
     </form>
     """
@@ -103,14 +141,16 @@ defmodule SubtleWeb.SubtleLive do
   def render_guesses(assigns) do
 #    IO.inspect(Puzzle.normalized_guesses(assigns.puzzle))
     ~H"""
-    <div class="flex rounded-lg p-4 bg-slate-700/50">
-      <div class="grid grid-flow-row auto-rows-max gap-y-2 md:gap-y-4">
+    <div class="flex rounded-lg p-4 justify-center bg-slate-700/50">
+      <div class="grid grid-flow-row justify-center auto-rows-max gap-y-2 md:gap-y-4">
         <%= for guess <- Game.guesses(@game) do %>
           <%= render_guess guess %>
         <% end %>
         <p class="text-zinc-200 ml-3"> <%= Game.guesses_remaining(@game) %> guesses remaining </p>
+
         <SubtleComponents.show_letters_used letters={Game.letters_used(@game)} />
       </div>
+
     </div>
     """
   end
@@ -128,27 +168,29 @@ defmodule SubtleWeb.SubtleLive do
     """
   end
 
-  def render_legend(assigns) do
+  def legend(assigns) do
     ~H"""
     <div class="legend">
       <h1>Legend:</h1>
-      <div class="legend_group">
-        <h2>Guesses</h2>
-        <.render_legend_key letter="!" hint={:correct} description="Correct letter" />
-        <.render_legend_key letter="?" hint={:wrong_position} description="Good letter, wrong location" />
-        <.render_legend_key letter="x" hint={:wrong_letter} description="Letter not in answer" />
-      </div>
-      <div class="legend_group">
-        <h2>Letters used</h2>
-        <.render_legend_key letter="x" hint={:used} description="Somewhere in puzzle" />
-        <.render_legend_key letter="x" hint={:wrong_letter} description="Letter not in answer" />
-        <.render_legend_key letter="x" hint={:unused} description="Not guessed yet" />
+      <div class="legend_groups">
+        <div class="legend_group">
+          <h2>Guesses</h2>
+          <.legend_key letter="!" hint={:correct} description="Correct letter" />
+          <.legend_key letter="?" hint={:wrong_position} description="Good letter, wrong location" />
+          <.legend_key letter="x" hint={:wrong_letter} description="Letter not in answer" />
+        </div>
+        <div class="legend_group">
+          <h2>Letters used</h2>
+          <.legend_key letter="x" hint={:used} description="Somewhere in puzzle" />
+          <.legend_key letter="x" hint={:wrong_letter} description="Letter not in answer" />
+          <.legend_key letter="x" hint={:unused} description="Not guessed yet" />
+        </div>
       </div>
     </div>
     """
   end
 
-  def render_legend_key(assigns) do
+  def legend_key(assigns) do
     ~H"""
     <div class="legend_key">
       <SubtleComponents.letter_box kind={:key} letter={@letter} hint={@hint}/>
@@ -157,15 +199,25 @@ defmodule SubtleWeb.SubtleLive do
     """
   end
 
-  def handle_event("guess", %{"guess" => guess} = params, socket) do
+  def available_answers(game) do
+    answers = Game.available_words_as_string(game, 20)
+    if answers == "" do
+      "You haven't made a guess yet!"
+    else
+      answers
+    end
+  end
+
+  def handle_event("guess", %{"guess" => guess}, socket) do
     guess =
       guess
       |> String.trim
       |> String.downcase
 
     # !! hack gets us true/false from nil, "true"
-    verify = !!Map.get(params, "verify", false)
-    game = %{socket.assigns.game | verify_guesses: verify}
+#    verify = !!Map.get(params, "verify", false)
+#    game = %{socket.assigns.game | verify_guesses: verify}
+    game = socket.assigns.game
 #    IO.inspect(guess, label: "guess")
 #    IO.inspect(verify, label: "verify")
 #    IO.inspect(game)
@@ -179,8 +231,20 @@ defmodule SubtleWeb.SubtleLive do
     end
   end
 
+  def handle_event("settings", %{"verify" => verify} = _params, socket) do
+    game = Game.set_verify(socket.assigns.game, to_boolean(verify))
+    {:noreply, assign(socket, game: game)}
+  end
+
   def handle_event("new_game", _params, socket) do
     {:noreply, assign(socket, game: Game.new())}
   end
+
+  def handle_event("give_up", _params, socket) do
+    {:noreply, assign(socket, game: Game.game_over_man(socket.assigns.game))}
+  end
+
+  defp to_boolean("true"), do: true
+  defp to_boolean(_), do: false
 
 end
